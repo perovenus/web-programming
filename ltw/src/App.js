f0597a8307f9cc4e5c7760d4d9d343474d9d1118
Kyo1006 <92916283+Kyo1006@users.noreply.github.com>
2022-11-10 09:12:30 +0700

    header implement

--- ltw/src/App.js
+++ ltw/src/App.js
@@ -1,6 +1,13 @@
 import Home from "./pages/home";
+import Header from "./components/header/Header";
+import CategoriesBar from "./components/categories-bar/CategoriesBar";
+
 function App() {
-  return <Home />;
+  return (
+    <>
+      <header><Header /></header>
+    </>
+  );
 }
 
 export default App;
---
+++ ltw/src/components/categories-bar/CategoriesBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function CategoriesBar() {
+  return (
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+    </ul>
+  )
+}
