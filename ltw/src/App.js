b7991a57db3374e1717d555ed59dc25a7e0cec84
Kyo1006 <92916283+Kyo1006@users.noreply.github.com>
2022-11-14 10:15:57 +0700

    product-detail implement

--- ltw/src/App.js
+++ ltw/src/App.js
@@ -2,6 +2,8 @@ import Category from "./components/category/category";
 import Carousel from "./components/carousel/carousel";
 import background from "./assets/images/background.jpg";
 import News from "./pages/news/News";
+import ProductDetail from "./pages/product-detail/ProductDetail";
+import CategoriesBar from "./components/categories-bar/CategoriesBar";
 
 function App() {
   var images = [
@@ -14,7 +16,7 @@ function App() {
   ];
 
   return (
-    <News />
+    <ProductDetail />
   )
 
 }
