2c891a39eac40e716a15fdbbb274145920351e32
Perovenus <69292119+perovenus@users.noreply.github.com>
2022-11-30 10:20:46 +0700

    debug: carousel

--- ltw/src/components/carousel/carousel.js
+++ ltw/src/components/carousel/carousel.js
@@ -1,17 +1,18 @@
 import "./carousel.css";
 import Item from "../item/item";
+import product from "../../assets/images/sample.webp";
 function Carousel(props) {
   return (
     <div id="main-carousel">
-      <div class="col-12">
+      <div>
         <h4>
           <span>{props.title}</span>
         </h4>
       </div>
-      <div class="owl-carousel owl-theme">
-        {props.images.map((image, index) => (
+      <div class="owl-carousel owl-theme" id="body-carousel">
+        {props.infos.map((image, index) => (
           <div class="item" key={index}>
-            <Item image={image} />
+            <Item image={image["image"]} />
           </div>
         ))}
       </div>
--- ltw/src/pages/home/home.js
+++ ltw/src/pages/home/home.js
@@ -1,44 +1,49 @@
+import "./home.css";
 import Carousel from "../../components/carousel/carousel";
 import Banner from "../../components/banner/banner";
-import background from "../../assets/images/background.jpg";
-import background1 from "../../assets/images/background.svg";
-import "./home.css";
 import banner2 from "../../assets/images/banner2.webp";
 import banner3 from "../../assets/images/banner3.webp";
-import Header from "../../components/header/Header";
+import axios from "axios";
 import product from "../../assets/images/sample.webp";
-import Footer from "../../components/footer/footer";
+import { useState, useEffect } from "react";
 function Home() {
-  const images = [
-    banner3,
-    banner2,
-    banner3,
-    banner3,
-    banner2,
-    banner3,
-    banner3,
-    banner2,
-    banner3,
-    banner3,
-    banner2,
-    banner3,
-  ];
-  const products = [
-    product,
-    product,
-    product,
-    product,
-    product,
-    product,
-    product,
-  ];
+  const images = [banner3, banner2, banner3];
+  const [news, setNews] = useState([]);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost/controllers/news.controller.php", {
+        params: {
+          action: 0,
+        },
+      })
+      .then((res) => {
+        setNews(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    axios
+      .get("http://localhost/controllers/products.controller.php", {
+        params: {
+          action: 0,
+        },
+      })
+      .then((res) => {
+        setProducts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  const pts = [product, product, product, product];
   return (
     <div className="home container">
       <Banner images={images} />
       <div className="col" id="home-body">
         <div className="row" id="home-content">
-          <Carousel images={products} title={"Tin mới"} />
-          <Carousel images={products} title={"Sản phẩm bán chạy"} />
+          <Carousel infos={news} title={"Tin mới"} />
+          <Carousel infos={products} title={"Sản phẩm bán chạy"} />
         </div>
       </div>
     </div>
